c96a7d9b40ab9ab0427a3f1825ade1653b76a13b
Maike Meuris <maike.meuris@student.ehb.be>
2018-05-09 15:28:00 +0200

    Link naar detail foto aanpassen

---
+++ Maike_Meuris_2MAWA_werkstuk1/FirstViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FirstViewController.swift
+//  Maike_Meuris_2MAWA_werkstuk1
+//
+//  Created by Maike Meuris on 29/04/2018.
+//  Copyright © 2018 Maike Meuris. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FirstViewController: UIViewController, MKMapViewDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //Wordt opgeroepen als locatie wordt geupdate, dus gaat niet bij tab 2, daar moet de code na het toevoegen van de annotations
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+}
+
+
